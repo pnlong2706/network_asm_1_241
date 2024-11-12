ec28d09f618f6efa3c8bdfba7ea17099a3bacf97
Pham Ngoc Long <77040324+pnlong2706@users.noreply.github.com>
2024-11-12 18:03:25 +0700

    long

--- client/main.py
+++ client/main.py
@@ -2,25 +2,45 @@ import socket
 import shlex
 import threading
 import json
+import random
+import string
 
-def connect_to_tracker_server(host, port):
+def connect_to_tracker_server(host, port, client_id):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((host, port))
-        
+    
         ### Send infomation to server
-        
+        with open('metafile_status.json', 'r') as file:
+            data = json.load(file)
+            
+            request = {
+                "action": "update",
+                "event": "started",
+                "infohash": [],
+                "peerid": client_id,
+                "port": 3000
+            }
+            
+            
+            for info_hash in data:
+                request["infohash"].append({"hash": data[info_hash]["infohash"], "downloaded": 100})
+            
+            s.send(json.dumps(request).encode())
+            s.recv(4096)
+                
         ######################
         
         print(f"Connect to tracker host {host}:{port}")
         return s
     
     except Exception as e:
+        print(e)
         return 0
 
-def close_connection(s):
+def close_connection(s, client_id):
     ## Send close connection
-    
+    s.send(json.dumps({"action": "none", "event": "stop", "peerid": client_id, "port": 3000}).encode())
     #####################
     s.close()
     
@@ -28,9 +48,11 @@ if __name__ == "__main__":
     SERVER_HOST = "127.0.0.1"
     SERVER_PORT = 6888
     
+    client_id = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+    
     print("Welcome to P2P file sharing, type 'help' for more infomation!")
     
-    s = connect_to_tracker_server(SERVER_HOST, SERVER_PORT)
+    s = connect_to_tracker_server(SERVER_HOST, SERVER_PORT, client_id)
     if(s): print(f"You are connected to tracker server {SERVER_HOST}: {SERVER_PORT}\nType 'connect [hostname] [port]' to connect to other tracker server!")
     else: print("Your are not connected to tracker server!\nType 'connect [hostname] [port]' to connect to other tracker server!")
     
@@ -46,18 +68,18 @@ if __name__ == "__main__":
                 close_connection(s)
             
             if(len(cmd)!=3):
-                s = connect_to_tracker_server(SERVER_HOST, SERVER_PORT)
+                s = connect_to_tracker_server(SERVER_HOST, SERVER_PORT, client_id)
             else:
                 SERVER_HOST = cmd[1]
                 SERVER_PORT = int(cmd[2])
-                s = connect_to_tracker_server(cmd[1], int(cmd[2]))
+                s = connect_to_tracker_server(cmd[1], int(cmd[2]), client_id)
                 
             if(not s):
                 print("Cannot connect to this host!")
 
         elif(cmd[0] == "exit"):
             if(s):
-                close_connection(s)
+                close_connection(s,client_id)
             break
         
         elif(cmd[0] == 'create'):
@@ -80,6 +102,11 @@ if __name__ == "__main__":
             ## perform bittorent handshake
             ## send request to get pieces
             s.send("get_peer".encode())
+            
+        elif(cmd[0] == "test"):
+            s.send(json.dumps({"action": "get-peer", "infohash": "anan221r5naan"}).encode())
+            res = s.recv(4096).decode()
+            print(res)
         
         else:
             print("Command is invalid!")
--- tracker/database.py
+++ tracker/database.py
@@ -6,13 +6,26 @@ conn = psycopg2.connect(database = "neondb",
                         password = "mxCvR9T0OboZ",
                         port = 5432)
 
-def hello():
-    cur = conn.cursor()
-    
-    #cur.execute("INSERT INTO metafile (name, infohash, description) VALUES ('catto', 'njajnvsknanksnkjj', 'Cute cate pic')")
-    #conn.commit()
-    
-    cur.execute("SELECT * FROM metafile")
+cur = conn.cursor()
+
+def update_peerfile(infohash, peerid, addr, port, downloaded):
+    cur.execute(f"INSERT INTO peerfile (infohash, peerid, peeraddr, peerport, downloaded) VALUES ('{infohash}', '{peerid}', '{addr}', {port}, {downloaded})")
+    conn.commit()
+
+def get_peer(infohash):
+    cur.execute(f"SELECT peerid, peeraddr, peerport FROM peerfile WHERE infohash = '{infohash}'")
     rows = cur.fetchall()
+    return rows
+
+def delete_peer(peerid, addr, port):
+    cur.execute(f"DELETE FROM peerfile WHERE peerid = '{peerid}' AND peeraddr = '{addr}' AND peerport = '{port}'")
+    conn.commit()
     
-    print(rows[0])
+def update_metafile(name, infohash, description):
+    cur.execute(f"INSERT INTO metafile (name, infohash, description) VALUES ('{name}', '{infohash}', '{description}'")
+    conn.commit()
+    
+if __name__ == "__main__":
+    update_peerfile("anan221r5naan", "uytuyiuac", "127.0.0.1", 3000)
+    print("Done")
+    
